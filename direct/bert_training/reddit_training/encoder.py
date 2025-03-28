bbdce60f46ab93e12cb1dbd959047078ef6b2adb
sk3feel <kraminkarim@mail.ru>
2025-03-28 18:59:10 +0300

    reddit -model

---
+++ direct/bert_training/reddit_training/encoder.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import joblib
+import os
+
+train_path = "../../data/reddit_data/train_augmented.csv"
+model_dir = "./bert_disease_model"
+
+df = pd.read_csv(train_path)
+
+le = LabelEncoder()
+le.fit(df['disease'])
+
+joblib.dump(le, os.path.join(model_dir, "label_encoder.joblib"))
+print("✅ LabelEncoder успешно сохранён!")
